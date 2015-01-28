86fdf5f200d1b49b06641bc5df1f0babb7a5b47c
TheBlueJeans <TheBlueJeans@users.noreply.github.com>
2015-01-28 23:11:29 +0800

    Create q1_fahrenheit_to_celsius.py

---
+++ p01/q1_fahrenheit_to_celsius.py
@@ -0,0 +1,3 @@
+Fahrenheit = input("Please enter the temperature in degree Fahrenheit with 2 decimal places: \n")
+Celcius = round(5/9*(float(Fahrenheit)-32), 2)
+print("The temperature in degree Celcius is:", Celcius)
