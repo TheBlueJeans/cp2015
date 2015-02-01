ec9941ce533779fc232140ed4f7f27f1bc47681d
TheBlueJeans <TheBlueJeans@users.noreply.github.com>
2015-02-01 11:34:05 +0800

    Update q12_find_factors.py

--- p02/q12_find_factors.py
+++ p02/q12_find_factors.py
@@ -1,7 +1,8 @@
 number = int(input("Please enter number: \n"))
 factors = ""
 for i in range (2, int(number/2)+1):
-    if number%i==0:
+    while number%i==0:
         factors = factors+"{0}, ".format(i)
         number=number/i
 print(factors)
+
