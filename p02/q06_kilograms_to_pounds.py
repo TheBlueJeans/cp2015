776b20c1bc35453da14ab39e0f447f7251ca2057
TheBlueJeans <TheBlueJeans@users.noreply.github.com>
2015-02-08 12:43:44 +0800

    Update q06_kilograms_to_pounds.py

--- p02/q06_kilograms_to_pounds.py
+++ p02/q06_kilograms_to_pounds.py
@@ -1,4 +1,4 @@
 table="Kilograms Pounds\n"
 for i in range(1, 11):
-    table=table+"{0}  \t  {1}\n".format(int(i), int(i*2.2))
+    table=table+"{0}  \t  {1:<10.1f}\n".format(int(i), i*2.2)
 print(table)
