f04fd4d1ea610744458e3a5ba56c1cdb79e28efe
TheBlueJeans <TheBlueJeans@users.noreply.github.com>
2015-02-08 12:09:36 +0800

    Create q3_find_gcd.py

---
+++ p03/q3_find_gcd.py
@@ -0,0 +1,8 @@
+def gcd(m, n):
+    for i in range(1, min(m, n)+1):
+        if m%i == 0 and n%i == 0:
+            gcd = i
+    print(gcd)
+int1 = int(input("Please enter an integer\n"))
+int2 = int(input("Please enter another integer\n"))
+gcd(int1, int2)
