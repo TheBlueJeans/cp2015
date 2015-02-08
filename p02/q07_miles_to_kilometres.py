2c0b8eb951ca632f5ec450fd45bb1495b5e5b4ed
TheBlueJeans <TheBlueJeans@users.noreply.github.com>
2015-02-08 12:45:01 +0800

    Update q07_miles_to_kilometres.py

--- p02/q07_miles_to_kilometres.py
+++ p02/q07_miles_to_kilometres.py
@@ -1,4 +1,4 @@
-table="Miles\tKilometres\tKilometres\tMiles\n"
+table = "Miles\tKilometres\tKilometres\tMiles\n"
 for i in range(1, 11):
-    table=table+"{0}\t{1}\t\t{2}\t\t{3}\n".format(int(i), round(i*1.609, 3), 20+(i-1)*5, round((20+(i-1)*5)/1.609, 3))
+    table = table+"{0}\t{1:<0.3f}\t\t{2}\t\t{3:<0.3f}\n".format(int(i), i*1.609, 20+(i-1)*5, (20+(i-1)*5)/1.609)
 print(table)
