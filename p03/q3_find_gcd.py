c76608f0dad97237b4e128610ef417ee3f5b77a2
TheBlueJeans <TheBlueJeans@users.noreply.github.com>
2015-02-12 09:29:56 +0800

    Update q3_find_gcd.py

--- p03/q3_find_gcd.py
+++ p03/q3_find_gcd.py
@@ -2,7 +2,7 @@ def gcd(m, n):
     for i in range(1, min(m, n)+1):
         if m%i == 0 and n%i == 0:
             gcd = i
-    print(gcd)
+    return gcd
 int1 = int(input("Please enter an integer\n"))
 int2 = int(input("Please enter another integer\n"))
-gcd(int1, int2)
+print(gcd(int1, int2))
